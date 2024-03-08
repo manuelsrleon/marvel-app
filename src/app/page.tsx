2b5815b20171d96c0ddd93326ef4e610914197c8
manuel.santamarina <manuel.santamarina@merlinsoftware.es>
2024-03-08 13:00:10 +0100

    https fix test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import TopBar from "./TopBar";
 function App() {
   return (
     <>
+      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       <TopBar />
       <HomePage></HomePage>
     </>
